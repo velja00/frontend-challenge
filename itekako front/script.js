d5388d8472de41e2de1b895b2aaff30192071f9b
Marko Nikolic <marko@piperhq.co>
2023-03-01 23:10:23 +0100

    Code review comments

--- itekako front/script.js
+++ itekako front/script.js
@@ -1,23 +1,28 @@
-
-
+// remove empty space
+// remove empty space
 const loader = document.querySelector("#loading");
-
+// remove empty space
 
 function displayLoading() {
+    // Name of css classes should not be named as css properties
   loader.classList.add("display");
+  // This is totally wrong approach. You should never use timeout for hiding loader.
+    // Hide loader when are sure that data is retrieved from backend API endpoint.
   setTimeout(() => {
       loader.classList.remove("display");
   }, 5000);
+  // What will happend  with loader in cases when api call takes more or less than 5000ms? user will see loader and offices at the same time.
 }
 
 function hideLoading() {
   loader.classList.remove("display");
 }
-
+// remove useless empty line
 
 async function fetchAsync () {    
   displayLoading();
-  let response = await fetch("https://itk-exam-api.herokuapp.com/api/offices");   
+  // response and data variables must be const, use let only in case where its needed.
+  let response = await fetch("https://run.mocky.io/v3/7e49690a-fa76-4ac5-ac19-054f9f10d857?mocky-delay=400ms");
   let data = await response.json();
   return data;    
 }
@@ -30,43 +35,49 @@ window.addEventListener('load', (event) => {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
+ //
   fetchAsync()
   .then(function (data) {
+      // add null  check and handle zero result (anyway you should cover case data.length === 0
     data.forEach((data) => {
+        // useless id, description, photo variables
        const {id, name, description, latitude, longitude, photo} = data;
-       var marker = L.marker([latitude, longitude]).addTo(map);
-       var place = name.split(" ");
+       var marker = L.marker([latitude, longitude]).addTo(map); // use const
+       var place = name.split(" "); // use const
        marker.bindPopup(`<b> ${name} </b><br>Itekako office in ${place[0]}.`)
   })
   })
 
+    // This is unacceptable. you shod NEVER call same endpoint twice retrieve totally same data.
   fetchAsync()
   .then(function (data) {
-    let loader = `<div class="boxLoading"></div>`;
+    let loader = `<div class="boxLoading"></div>`; // use const
     document.getElementById('offices').innerHTML = loader;
     let result = '';
-    let i=0;
+    let i=0;  // totally useless counter forEach has index as second parameter https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
     data.forEach((data) => {
       hideLoading();
-      document.getElementById("mapcontainer").style.display = "none";
-      const {id, name, description, latitude, longitude, photo} = data;
+      // never locate element selector in loop (foreach in this case).
+      document.getElementById("mapcontainer").style.display = "none";  // how meny time  your going to hide this element? :-)  also hide element by add/rremove css class
+      const {id, name, description, latitude, longitude, photo} = data; // useless variables  latitude and longitude
+        // Some of offices have no image so don't  render img tag in all cases. Add condition which will check is photo is  null or not.
       result +=
           `<div class="div-office-${id} office-items">
               <ul>
-                  <img class="circular_image" src="${photo}" alt="${name}">
+                  <img class="circular_image" src="${photo}" alt="${name}"> 
                   <div id="profileImage" class="div${i}${photo==null? ' setindex':''}">${name.charAt(0)}</div>
                   <li><h4> ${name}<h4></li>
                   <li> ${description}</li>
               </ul>
               <hr>
           </div>`;
-          document.getElementById('offices').innerHTML = result;
-          i++;
+          document.getElementById('offices').innerHTML = result; // This is unacceptable. You should render result only once outside of loop (forEach)
+          i++; // remove counter
   })})
 
 });
 
-
+// from this to end all code is totally useless. just think about better solution and DRY principe https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
 const grid= document.querySelector(".grid");
 
 grid.addEventListener('click',function () {
@@ -134,6 +145,7 @@ list.addEventListener('click',function () { fetchAsync()
 const mapa= document.querySelector(".mapa");
 
 mapa.addEventListener('click',function () {
+    // why do you need variable div
  const div= document.getElementById('offices').innerHTML = "";
  document.getElementById("mapcontainer").style.display = "block";
  document.getElementById("mapcontainer").style.visibility = "visible";
